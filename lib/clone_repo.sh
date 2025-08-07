24395b012bcfa5f206b5887c432263e8ebc9789d
Tenergy Innovation <45764087+tenergyinnovation@users.noreply.github.com>
2025-08-07 16:10:25 +0700

    Update  Add Debounce library for switch and button

--- lib/clone_repo.sh
+++ lib/clone_repo.sh
@@ -10,4 +10,12 @@ TARGET_DIR="repository"
 # Clone the repository
 git clone $REPO_URL1
 
+# Clone Debounce library to specific location in tiny32_v3
+echo "Cloning Debounce library to tiny32_v3/src/Debounce..."
+cd tiny32_v3/src || exit
+git clone "https://github.com/wkoch/Debounce.git"
+cd ../../
+
+echo "Repositories cloned successfully."
+
 
